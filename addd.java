31d22061ee41f4cc2d6bcb026abbfe3f4b039cae
Yuvraj-shinde <yuvrajshinde513@gmail.com>
2024-05-16 10:59:19 +0530

    added

---
+++ addd.java
@@ -0,0 +1,17 @@
+public class addd {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a number:");
+		n = sc.nextInt();
+
+		if (n % 2 == 0) {
+			System.out.println("the given number " + n + " is even");
+		} else {
+			System.out.println("the given numer " + n + " is odd");
+		}
+
+	}
+
+}
