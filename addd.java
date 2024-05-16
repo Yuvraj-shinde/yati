fd5a752a4d78f6e4b569bd3b79989d7ea0fe278f
Yuvraj-shinde <yuvrajshinde513@gmail.com>
2024-05-16 11:16:39 +0530

    added

--- addd.java
+++ addd.java
@@ -16,8 +16,9 @@ public class addd {
 		System.out.println("the given numer " + n + " is odd");
         System.out.println("the given numer " + n + " is odd");
 		System.out.println("the given numer " + n + " is odd");
-        System.out.println("the given numer " + n + " is odd");
-		System.out.println("the given numer " + n + " is odd");
+
+        System.out.println("thofhsioghsgpispjfpjgofsjgvj ");
+        
 
 	}
 
